8f6bfa63a8f16d2db7eabf54788c939e9014f207
ai-compare <67952486+ai-compare@users.noreply.github.com>
2020-10-21 10:59:15 +0200

    Update AI-Compare-Sentiment_Analysis-python-request.py

--- AI-Compare-Sentiment_Analysis-python-request.py
+++ AI-Compare-Sentiment_Analysis-python-request.py
@@ -1,15 +1,15 @@
 import requests
 
-# You can find the documentation here : https://documenter.getpostman.com/view/10011301/SzmiVFqh?version=latest#071c4411-5aac-47b6-bf66-fb4eb374db35
+# You can find the documentation here : https://www.ai-compare.com/v1/redoc/#operation/Sentiment%20Analysis
 
 #Enter your API Token
-headers = {'x-access-token': 'Enter your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
+headers = {'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
 
 # Select API
-url = 'https://www.ai-compare.com/api/v1/text/create/compare/sentiment_analysis'
+url = 'https://www.ai-compare.com/v1/pretrained/text/sentiment_analysis'
 
 # Select providers, and text to analyze
-payload = {'providers': '[\'google_cloud\', \'cognitives_service\', \'aws\']','text':'I am angry today', 'sentiments_to_find': 'neutral','language': 'en-US'}
+payload = {'providers': '[\'google_cloud\', \'cognitives_service\', \'aws\', \'ibm\']','text':'I am angry today', 'sentiments_to_find': 'neutral','language': 'en-US'}
 
 # Request to AI COMPARE
 response = requests.request("POST", url, headers=headers, data = payload, files = files)
